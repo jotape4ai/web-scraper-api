1a4ecea1dad4f23d70ad2a38972d06593edd8b80
jotape4ai <jotape4ai@gmail.com>
2025-06-22 21:56:47 -0300

    📦 Add SDK main export module with clean re-exports

---
+++ sdk/src/index.ts
@@ -0,0 +1,6 @@
+export { WebScraperApiClient } from './client';
+export * from './types';
+
+// Re-export for convenience
+import { WebScraperApiClient } from './client';
+export default WebScraperApiClient;
